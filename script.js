4f18d3960ab2b5ddfca7119becc90a0f7db597ec
Honour-Eboiye <honoureboiye@gmail.com>
2024-11-11 10:08:16 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let y = 100;
+let x = 30;
+
+const sum = y + x;
+console.log(sum);
+
+const sub = y - x;
+console.log(sub);
+
+const mult = y * x;
+console.log(mult);
+
+const division = y / x;
+console.log(division);
+
+const modulus = y % x;
+console.log(modulus);
+
+const exponential = y**x;
+console.log(exponential);
