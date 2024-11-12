9b61c0c79d1e1260c96d450d0a3d41428a8909b8
honoureboiye <honoureboiye@gmail.com>
2024-11-12 14:58:26 +0100

    Last Updated 12/11/2024

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let y = 100;
-let x = 30;
+/**
+  let y = 100;
+  let x = 30;
 
-const sum = y + x;
-console.log(sum);
+  const sum = y + x;
+  console.log(sum);
 
-const sub = y - x;
-console.log(sub);
+  const sub = y - x;
+  console.log(sub);
 
-const mult = y * x;
-console.log(mult);
+  const mult = y * x;
+  console.log(mult);
 
-const division = y / x;
-console.log(division);
+  const division = y / x;
+  console.log(division);
 
-const modulus = y % x;
-console.log(modulus);
+  const modulus = y % x;
+  console.log(modulus);
 
-const exponential = y**x;
-console.log(exponential);
+  const exponential = y**x;
+  console.log(exponential); 
+ */
+
+  let calculation = `1 + 1`;
+  console.log(calculation);
